27ca3add804b1758d25b8168fa98c231bcdbdf6d
x91003502 <x91003502@gmail.com>
2021-07-10 19:02:03 +0800

    Added DFSSolver function and test cases.

--- fifteen_puzzle/puzzle_solver.py
+++ fifteen_puzzle/puzzle_solver.py
@@ -1,4 +1,5 @@
 from collections import deque
+from os import stat
 from fifteen_puzzle.puzzle_solver_util import *
 from fifteen_puzzle.impossible_fifteen_puzzle import check_solvability
 import logging
@@ -18,17 +19,63 @@ def BFSSolver(initial, goal):
         if state == goal:
             # return prev
             break
-        predecessor = prev[list_to_str(state)]
+        # predecessor = prev[list_to_str(state)]
         successors = compute_successors(state, initial=initial)
         for suc in successors:
-            LOGGER.debug(f'Successors state {suc}  Valid {suc!=predecessor}')
-            if suc != predecessor:
+            # LOGGER.debug(f'Successors state {suc}  Valid {suc!=predecessor}')
+            if list_to_str(suc) not in prev:
                 prev[list_to_str(suc)] = state
                 Q.append(suc)
             else:
                 LOGGER.debug('Duplicate')
     
     return construct_moves(state, prev)
+
+
+def DFSSolver(initial, goal, max_depth):
+    BLANK = 16
+    S = deque()
+    S.append(initial)
+    
+    initial_str= list_to_str(initial)
+    prev = dict()
+    prev[initial_str] = None
+    
+    visited = dict()
+    visited[initial_str] = False
+    
+    depth = dict()
+    depth[initial_str] = 0
+    state = None
+    while len(S) > 0:
+        state = S.pop()
+        state_str = list_to_str(state)
+        if visited[state_str] is False:
+            visited[state_str] = True
+            if state == goal:
+                LOGGER.debug('Found goal')
+                break
+            if depth[state_str] > max_depth:
+                # LOGGER.debug('Maximum depth')
+                continue
+            else:
+                successors = compute_successors(state, initial=initial)
+                for suc in successors:
+                    suc_str = list_to_str(suc)
+                    # if suc_str not in visited or visited[suc_str] is False:
+                    if suc_str not in visited:
+                        visited[suc_str] = False
+                        prev[suc_str] = state
+                        depth[suc_str] = depth[state_str] + 1
+                        # LOGGER.debug(f'depth {depth[suc_str]}')
+                        S.append(suc)
+                    # else:
+                    #     LOGGER.debug('Duplicate')
+    if state != goal:
+        LOGGER.warning('Not found goal')
+        return DFSSolver(initial, goal, int(max_depth * 1.1))
+    else:
+        return construct_moves(state, prev)
 
 if __name__ == '__main__':
     max_degree = 11
--- fifteen_puzzle/test_puzzle_solver.py
+++ fifteen_puzzle/test_puzzle_solver.py
@@ -9,5 +9,7 @@ def test_BFSSolver1():
         max_degree = 5
         initial = generate_puzzle(max_degree)
         goal = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-        sol, _ = BFSSolver(initial, goal)
-        assert sol == goal
+        sol1, _ = BFSSolver(initial, goal)
+        sol2, _ = DFSSolver(initial, goal, max_degree)
+        assert sol1 == goal
+        assert sol2 == goal
